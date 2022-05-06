130d626ab8bdea04e9820532d0c8411bd38ae664
jungsiroo <fordevelop@pm.me>
2022-05-06 07:23:09 +0000

    Feature : Unite Train / Valid task together

--- arguments.py
+++ arguments.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from typing import Optional
+from transformers import TrainingArguments
 
 
 @dataclass
@@ -90,3 +91,26 @@ class DataTrainingArguments:
     use_faiss: bool = field(
         default=False, metadata={"help": "Whether to build with faiss"}
     )
+
+train_args = TrainingArguments(
+    do_train=True,
+    do_eval=True,
+    output_dir="./finetune/",
+    overwrite_output_dir=True,
+    evaluation_strategy="steps",
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    gradient_accumulation_steps=2,
+    learning_rate=1e-5,
+    num_train_epochs=8,
+    warmup_ratio=0.1,
+    logging_strategy="steps",
+    logging_steps=100,
+    save_strategy="steps",
+    save_steps=200,
+    save_total_limit=1,
+    seed=42,
+    eval_steps=200,
+    metric_for_best_model="exact_match",
+    load_best_model_at_end=True,
+)
--- eval_reader.sh
+++ eval_reader.sh
@@ -3,6 +3,4 @@
 # 직접 finetune 한 모델의 결과가 있다면 --model_name_or_path 에 불러주기
 
 python train.py \
---model_name_or_path="./finetune/" \
---output_dir="./finetune_result" \
---do_eval
+--model_name_or_path="./finetune/"
